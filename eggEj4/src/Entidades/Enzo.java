8114cd6227fd24a0b06fc1dfeea230a5e6c17833
Enzo Angileri <enzoangileri96@gmail.com>
2021-10-15 18:43:46 -0300

    agregue la clase enzo

---
+++ eggEj4/src/Entidades/Enzo.java
@@ -0,0 +1,10 @@
+
+package Entidades;
+
+public class Enzo {
+    
+    private String nombre;
+    private String apellido;
+    private int edad;
+    private String genero;
+}
--- eggEj4/src/eggej4/EggEj4.java
+++ eggEj4/src/eggej4/EggEj4.java
@@ -1,21 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package eggej4;
 
-/**
- *
- * @author cazad
- */
 public class EggEj4 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+       
     }
     
 }
